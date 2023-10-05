d5c4e4a1ea8e0ad9078506c40b0261a0867fb5b1
nirmal.c <nirmalwebdev@gmail.com>
2023-10-05 23:29:33 +0530

    css alignment fix

--- reeco/src/components/Productlist.js
+++ reeco/src/components/Productlist.js
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import avocado from "../Avocado Hass.jpg"
 import { editProduct } from "../redux/Action/actions";
 import "./Productlist.css";
 import debounce from "lodash.debounce";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  IconButton,
+} from '@mui/material';
+import { CheckCircle, Close, Print } from '@mui/icons-material';
 const ProductList = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
   const products = useSelector((state) => state.products);
   const dispatch = useDispatch();
+  const componentRef = useRef();
 
   
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   const debouncedFilterProducts = debounce((search) => {
     if (search) {
       const filtered = products.filter((product) =>
@@ -56,70 +73,102 @@ const ProductList = () => {
     dispatch(editProduct(editedProduct));
   };
 
+  const handlePrint = () => {
+    window.print();
+  };
+
+
   return (
     <div>
       <h2>Product List</h2>
-      <input
-        type="text"
-        placeholder="Search by Product Name"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
+      <TextField
+      placeholder="Search by Product Name"
+      value={searchTerm}
+      onChange={handleInputChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+     <Button
+          variant="contained"
+          color="primary"
+          onClick={handlePrint}
+          startIcon={<Print />}
+          style={{ marginLeft: '50px' }}
+        >
+        </Button>
       <div className="table-container">
-      <table className="product-table">
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Brand</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Total</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredProducts.map((product) => (
-            <tr key={product.id}>
-              <td>
-              <div className="product-info-container">
-                <img
-                  src={avocado}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <td>{product.name}</td>
-                </div>
-              </td>
-              
-              <td>{product.brand}</td>
-              <td>${product.price}</td>
-              <td>{product.quantity}</td>
-              <td>${product.total}</td>
-              <td
-                className={
-                  product.status === "Approved" ? "approved" : "missing"
-                }
-              >
-                {product.status}
-              </td>
-              <td>
-                <FontAwesomeIcon
-                  icon={faCheckCircle} 
-                  className="action-icon"
-                  onClick={() => handleApprove(product.id)}
-                />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className="action-icon action-icon-red"
-                  onClick={() => handleReject(product)}
-                />
+      
 
-                <button onClick={() => handleEdit(product)}>Edit</button>
-              </td>
-            </tr>
+<TableContainer component={Paper}  ref={componentRef}>
+      <Table className="product-table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Product Name</TableCell>
+            <TableCell>Brand</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Quantity</TableCell>
+            <TableCell>Total</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {filteredProducts.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>
+                <div className="product-info-container">
+                  <img
+                    src={avocado}
+                    alt={product.name}
+                    className="product-image"
+                  />
+                  {product.name}
+                </div>
+              </TableCell>
+              <TableCell>{product.brand}</TableCell>
+              <TableCell>${product.price}</TableCell>
+              <TableCell>{product.quantity}</TableCell>
+              <TableCell>${product.total}</TableCell>
+              <TableCell>
+                <span
+                  className={
+                    product.status === 'Approved' ? 'approved' : 'missing'
+                  }
+                >
+                  {product.status}
+                </span>
+              </TableCell>
+              <TableCell className="product-info-container"  style={{ width: '150px' }}>
+                <IconButton
+                  onClick={() => handleApprove(product.id)}
+                >
+                  <CheckCircle />
+                </IconButton>
+                <IconButton
+                  onClick={() => handleReject(product)}
+                >
+                  <Close color="error" />
+                </IconButton>
+                <span
+                  className="button-text"
+                  onClick={() => handleEdit(product)}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Edit
+                </span>
+              </TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
       </div>
     </div>
   );
